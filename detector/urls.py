bb73f0c437ae65c891193c41e37a9cd76fbba601
nagendrasrivastava <sanu.nagendra@gmail.com>
2017-08-01 21:44:13 +0530

    admin files which is not required for local env has been deleted ,index file added

--- detector/urls.py
+++ detector/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    url(r'^$', TemplateView.as_view(
+        template_name='index.html')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^super/v1/', include('super_detector.urls')),
 ]
